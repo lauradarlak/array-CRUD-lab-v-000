e165d82beaa5372e307113eba644fcb2b3b0185e
Laura Darlak <laura@polyporedesign.com>
2018-08-17 12:31:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,13 @@ def create_an_empty_array
 end
 
 def create_an_array
-alpha = ["a", "b", "c", "d"]
+  alpha = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
